46fe65f24e3e1e2899aad9bd4c62aed8abe72205
unknown <jikrilar2903@gmail.com>
2024-11-06 14:52:32 +0700

    first commit

---
+++ src/app/hero.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Image from "next/image";
+import { Input, Button, Typography } from "@material-tailwind/react";
+
+function Hero() {
+  return (
+    <header className="bg-white p-8">
+      <div className="container mx-auto grid h-full gap-10 min-h-[60vh] w-full grid-cols-1 items-center lg:grid-cols-2">
+        <div className="row-start-2 lg:row-auto">
+          <Typography
+            variant="h1"
+            color="blue-gray"
+            className="mb-4 lg:text-5xl !leading-tight text-3xl"
+          >
+            Bonia Auction
+          </Typography>
+          <Typography
+            variant="lead"
+            className="mb-4 !text-gray-500 md:pr-16 xl:pr-28"
+          >
+            "Own Exclusive Fashion with Your Best Bid"
+            <br />
+            <span className="italic">
+              Step into our auction world, where every bid brings you closer to
+              your dream collection. Classy fashion, limited opportunities.
+            </span>
+          </Typography>
+          <div className="grid">
+            <Typography
+              variant="small"
+              className="mb-2 text-gray-900 font-medium"
+            >
+              Your email
+            </Typography>
+            <div className="mb-2 flex w-full flex-col gap-4 md:w-10/12 md:flex-row">
+              {/* @ts-ignore */}
+              <Input color="gray" label="Enter your email" size="lg" />
+              <Button color="gray" className="w-full px-4 md:w-[12rem]">
+                require offer
+              </Button>
+            </div>
+          </div>
+          <Typography variant="small" className="font-normal !text-gray-500">
+            Read my{" "}
+            <a href="#" className="font-medium underline transition-colors">
+              Terms and Conditions
+            </a>
+          </Typography>
+        </div>
+        <Image
+          width={1024}
+          height={1024}
+          alt="team work"
+          src="/image/image-7.svg"
+          className="h-[36rem] w-full rounded-xl object-cover"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Hero;
---
+++ src/components/project-card.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
+
+interface ProjectCardProps {
+  img: string;
+  title: string;
+  desc: string;
+}
+
+export function ProjectCard({ img, title, desc }: ProjectCardProps) {
+  return (
+    <Card color="transparent" shadow={false}>
+      <CardHeader floated={false} className="mx-0 mt-0 mb-6 h-80 rounded-none">
+        <Image
+          src={img}
+          alt={title}
+          width={768}
+          height={1024}
+          className="h-full w-full"
+        />
+      </CardHeader>
+      <CardBody className="p-0">
+        <a
+          href="#"
+          className="text-blue-gray-900 transition-colors hover:text-gray-800"
+        >
+          <Typography variant="h5" className="mb-2">
+            {title}
+          </Typography>
+        </a>
+        <Typography className="mb-6 font-normal !text-gray-500">
+          {desc}
+        </Typography>
+        {/* <Button color="gray" size="sm">
+          see details
+        </Button> */}
+      </CardBody>
+    </Card>
+  );
+}
+
+export default ProjectCard;
